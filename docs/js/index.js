3a97360ba5149f50c2c2f52c09b2617ba453aa2d
Yosif Ibrahim <65606350+Uoerim@users.noreply.github.com>
2023-07-07 19:00:15 +0300

    Initial Commit (Resume created)
    Created the home page and the resume

---
+++ docs/js/index.js
@@ -0,0 +1,35 @@
+
+
+/* 
+ */
+
+
+
+
+
+
+const element = document.querySelector(".resume-section");
+element.addEventListener('wheel', (event) => {
+
+    document.querySelector(".buttom-section").scrollBy({
+        left: event.deltaY < 0 ? -400 : 400,
+        behavior: "smooth"
+
+    });
+});
+
+$(".resume-option").click(function (e) {
+    e.preventDefault();
+    $(".resume-section").css("transform", "translateX(0vw)");
+    $(".resume-section").css("visibility", "visible");
+});
+
+
+
+
+$(".top-menu > img").click(function (e) {
+    e.preventDefault();
+    $(".resume-section").css("transform", "translateX(100vw)");
+    $(".resume-section").css("visibility", "hidden");
+});
+
